ae80da256c4c43e3f829676bb77c62f92102f3cc
Carlos Augusto Daniel da Costa Rubim Leao - DATAPREVRJ <carlos.leao@dataprev.gov.br>
2020-01-09 15:37:46 -0300

    adding windows bath snipet

---
+++ windows_batch/swap_network_interfaces.bat
@@ -0,0 +1,22 @@
+@echo off
+
+SET netcard1=Ethernet
+SET netcard2=Wi-Fi
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+netsh int show int | find "%netcard2%"
+IF errorlevel 1 (
+	echo Does not exists a netcard named '%netcard2%'
+	pause
+) ELSE (
+	netsh int show int "%netcard2%" | find "Conectado"
+	IF errorlevel 1 (
+		netsh interface set interface name="%netcard1%" admin=DISABLED
+		netsh interface set interface name="%netcard2%" admin=ENABLED
+	) ELSE (
+		netsh interface set interface name="%netcard1%" admin=ENABLED
+		netsh interface set interface name="%netcard2%" admin=DISABLED
+	)
+)
+
