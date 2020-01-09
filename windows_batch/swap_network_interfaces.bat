4790a9bfd3e0558eb8030f81ab7a89b1d28a9af1
Carlos Augusto Daniel da Costa Rubim Leao - DATAPREVRJ <carlos.leao@dataprev.gov.br>
2020-01-09 19:12:05 -0300

    update in script

--- windows_batch/swap_network_interfaces.bat
+++ windows_batch/swap_network_interfaces.bat
@@ -5,18 +5,26 @@ SET netcard2=Wi-Fi
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
+netsh int show int | find "%netcard1%"
+IF errorlevel 1 (
+	echo Does not exists a netcard named '%netcard1%'
+	pause
+	exit 0
+)
+
 netsh int show int | find "%netcard2%"
 IF errorlevel 1 (
 	echo Does not exists a netcard named '%netcard2%'
-	pause
+	pause 
+	exit 0
+)
+
+netsh int show int "%netcard2%" | find "Conectado"
+IF errorlevel 1 (
+	netsh interface set interface name="%netcard1%" admin=DISABLED
+	netsh interface set interface name="%netcard2%" admin=ENABLED
 ) ELSE (
-	netsh int show int "%netcard2%" | find "Conectado"
-	IF errorlevel 1 (
-		netsh interface set interface name="%netcard1%" admin=DISABLED
-		netsh interface set interface name="%netcard2%" admin=ENABLED
-	) ELSE (
-		netsh interface set interface name="%netcard1%" admin=ENABLED
-		netsh interface set interface name="%netcard2%" admin=DISABLED
-	)
+	netsh interface set interface name="%netcard1%" admin=ENABLED
+	netsh interface set interface name="%netcard2%" admin=DISABLED
 )
 
